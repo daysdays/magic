023470d7d611b03a175094e124b07637d634552d
木风 <niluanxy@163.com>
2015-06-29 13:18:59 +0800

    优化打包脚本，去处无用代码

--- gulpfile.js
+++ gulpfile.js
@@ -22,34 +22,33 @@ gulp.task("dev-magic-js", function() {
     var LIB_MINJS = __dirname + "/src/lib/minjs/",
         DIR_CORE  = __dirname + "/src/core/";
 
-    return gulp.src(DIR_CORE + "main.js")
-        .pipe(webpack({
-                entry: DIR_CORE + "main.js",
-                output: {
-                    filename: "magic.js"
-                },
-                resolve: {
-                    alias: {
-                        util:  LIB_MINJS + "util.js",
-                        query: LIB_MINJS + "selector.js",
-                        director: LIB_MINJS + "director.js",
-                        domready: LIB_MINJS + "ondomready.js",
-                        extend:   LIB_MINJS + "extend.js",
-                        promise:  LIB_MINJS + "promise.js",
-                        jsonp  :  LIB_MINJS + "jsonp.js",
-                    }
-                },
-                module: {
-                    loaders: [
-                        { test: /\.html$/, loader: "html" },
-                        { test: /\.scss$/, loader: "style!css!sass!autoprefixer" }
-                    ]
+    webpack({
+            entry: DIR_CORE + "main.js",
+            output: {
+                filename: "magic.js"
+            },
+            resolve: {
+                alias: {
+                    util:  LIB_MINJS + "util.js",
+                    query: LIB_MINJS + "selector.js",
+                    director: LIB_MINJS + "director.js",
+                    domready: LIB_MINJS + "ondomready.js",
+                    extend:   LIB_MINJS + "extend.js",
+                    promise:  LIB_MINJS + "promise.js",
+                    jsonp  :  LIB_MINJS + "jsonp.js",
                 }
-            }))
-        .pipe(gulp.dest("dist/"))
-        .pipe(uglify())
-        .pipe(rename("magic.min.js"))
-        .pipe(gulp.dest("dist/"))
+            },
+            module: {
+                loaders: [
+                    { test: /\.html$/, loader: "html" },
+                    { test: /\.scss$/, loader: "style!css!sass!autoprefixer" }
+                ]
+            }
+        })
+    .pipe(gulp.dest("dist/"))
+    .pipe(uglify())
+    .pipe(rename("magic.min.js"))
+    .pipe(gulp.dest("dist/"))
 })
 
 gulp.task("clean", function() {
